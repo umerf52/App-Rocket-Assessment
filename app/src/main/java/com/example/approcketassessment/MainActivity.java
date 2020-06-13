fb3785ce9bb649d9debac2872bda710c15e0be90
Umer Farooq Zia <umer.farooq.042@gmail.com>
2020-06-13 21:21:30 +0500

    Link signup to DB

--- app/src/main/java/com/example/approcketassessment/MainActivity.java
+++ app/src/main/java/com/example/approcketassessment/MainActivity.java
@@ -1,19 +1,27 @@
 package com.example.approcketassessment;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final int LOGIN_REQUEST_CODE = 1;
+    public static final int SIGNUP_REQUEST_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         Button loginButton = findViewById(R.id.login_button);
         Button signupButton = findViewById(R.id.signup_button);
 
@@ -21,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, LOGIN_REQUEST_CODE);
             }
         });
 
@@ -29,8 +37,30 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, SIGNUP_REQUEST_CODE);
             }
         });
+    }
+
+    // Call Back method  to get the Message form other Activity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // check if the request code is same as what is passed  here it is 2
+        if (requestCode == SIGNUP_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                Toast.makeText(getApplicationContext(), "You can now login", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Some error occurred during sign up", Toast.LENGTH_LONG).show();
+            }
+        } else if (requestCode == LOGIN_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+//                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+//                intent.putExtra("username", "username");
+//                startActivity(intent);
+            } else {
+                Toast.makeText(getApplicationContext(), "Some error occurred during sign up", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/approcketassessment/SignupActivity.java
+++ app/src/main/java/com/example/approcketassessment/SignupActivity.java
@@ -1,18 +1,44 @@
 package com.example.approcketassessment;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class SignupActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        db = FirebaseFirestore.getInstance();
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setTimestampsInSnapshotsEnabled(true)
+                .build();
+        db.setFirestoreSettings(settings);
 
         final EditText username = findViewById(R.id.username);
         final EditText password = findViewById(R.id.password);
@@ -21,10 +47,62 @@ public class SignupActivity extends AppCompatActivity {
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String usernameText = username.getText().toString().trim();
-                String passwordText = password.getText().toString().trim();
-//                Toast.makeText(getApplicationContext(), usernameText + " " + passwordText, Toast.LENGTH_LONG).show();
+                final String usernameText = username.getText().toString().trim();
+                final String passwordText = password.getText().toString().trim();
+
+                db.collection("accounts")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        if (usernameText.equals(document.getString("username"))) {
+                                            returnError();
+                                        }
+                                    }
+                                    addUser(usernameText, passwordText);
+                                } else {
+                                    Log.w("signup", "Error getting documents.", task.getException());
+                                    returnError();
+                                }
+                            }
+                        });
             }
         });
+    }
+
+    void addUser(final String un, String pw) {
+        Map<String, Object> user = new HashMap<>();
+        user.put("username", un);
+        user.put("password", pw);
+
+        // Add a new document with a generated ID
+        db.collection("accounts")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Intent intent = new Intent();
+                        intent.putExtra("username", un);
+                        setResult(Activity.RESULT_OK, intent);
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("signup", "Failed");
+                        Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_LONG).show();
+                        Log.e("signup", "forloop");
+                        returnError();
+                    }
+                });
+    }
+
+    void returnError() {
+        Intent intent = new Intent();
+        setResult(Activity.RESULT_CANCELED, intent);
+        finish();
     }
 }
