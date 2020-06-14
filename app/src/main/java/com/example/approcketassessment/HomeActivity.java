76cc14476af7395c654b9036f59595da3d36a523
Umer Farooq Zia <umer.farooq.042@gmail.com>
2020-06-14 12:54:36 +0500

    Add clock and date

--- app/src/main/java/com/example/approcketassessment/HomeActivity.java
+++ app/src/main/java/com/example/approcketassessment/HomeActivity.java
@@ -1,8 +1,13 @@
 package com.example.approcketassessment;
 
 import android.os.Bundle;
+import android.widget.TextClock;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        TextClock clock = findViewById(R.id.clock);
+        clock.setFormat12Hour("hh:mm a");
+
+        TextView dateView = findViewById(R.id.date);
+        SimpleDateFormat formatter = new SimpleDateFormat("MMMM dd, yyyy");    //formating according to my need
+        String date = formatter.format(Calendar.getInstance().getTime());
+        dateView.setText(date);
     }
 }
