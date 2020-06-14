07d5959433218c72ad5c3536871f42b6733782cc
Umer Farooq Zia <umer.farooq.042@gmail.com>
2020-06-14 13:36:27 +0500

    Save login state

--- app/src/main/java/com/example/approcketassessment/MainActivity.java
+++ app/src/main/java/com/example/approcketassessment/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.approcketassessment;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        restoreLogin();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -55,13 +58,35 @@ public class MainActivity extends AppCompatActivity {
             }
         } else if (requestCode == LOGIN_REQUEST_CODE) {
             if (resultCode == Activity.RESULT_OK) {
-                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-                intent.putExtra("username", data.getStringArrayExtra("username"));
-                Toast.makeText(getApplicationContext(), "Logged In", Toast.LENGTH_LONG).show();
-                startActivity(intent);
+                String un = data.getStringExtra("username");
+
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("username", un);
+                editor.putBoolean("status", true);
+                editor.apply();
+
+                launchHome(un);
             } else {
                 Toast.makeText(getApplicationContext(), data.getStringExtra("msg"), Toast.LENGTH_LONG).show();
             }
         }
+    }
+
+    void restoreLogin() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String un = preferences.getString("username", "");
+        boolean status = preferences.getBoolean("status", false);
+
+        if (!un.equals("") && (status)) {
+            launchHome(un);
+        }
+    }
+
+    private void launchHome(String un) {
+        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+        intent.putExtra("username", un);
+        Toast.makeText(getApplicationContext(), "Logged In", Toast.LENGTH_LONG).show();
+        startActivity(intent);
     }
 }
